a4f0a14012c557f80819fee1b006773809023a32
unknown <josemanueltenchipe@gmail.com>
2024-06-08 16:26:13 -0600

    se añade login, register, forgetpass, general-view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GeneralViewComponent } from "./features/general-view/general-view.component";
+import { ForgetpassComponent } from './features/forgetpass/forgetpass.component';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'IAPEX_APP-WEB';
---
+++ src/app/features/general-view/general-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'general-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './general-view.component.html',
+  styleUrl: './general-view.component.css'
+})
+export class GeneralViewComponent {
+
+}
