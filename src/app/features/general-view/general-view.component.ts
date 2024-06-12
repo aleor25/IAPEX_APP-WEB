39ffd88afdd33098a259088122648d0f33ef1cfd
Ale <alex03ortizperez@gmail.com>
2024-06-12 13:03:26 -0600

    cambios en el general-view y su estructura en el sidebar y navbar.

--- src/app/features/general-view/general-view.component.ts
+++ src/app/features/general-view/general-view.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'general-view',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, NavbarComponent, RouterOutlet],
   templateUrl: './general-view.component.html',
   styleUrl: './general-view.component.css'
 })
