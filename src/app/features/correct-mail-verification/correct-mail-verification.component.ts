3fe6d60794415db70dd3220158e179e961a5a73d
unknown <carlins502@gmail.com>
2024-06-13 09:30:00 -0600

    se añadieron los nuevos archivos de correct-email-verification, reset-password, restore-password-2, verified-email

---
+++ src/app/features/correct-mail-verification/correct-mail-verification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-correct-mail-verification',
+  standalone: true,
+  imports: [],
+  templateUrl: './correct-mail-verification.component.html',
+  styleUrl: './correct-mail-verification.component.css'
+})
+export class CorrectMailVerificationComponent {
+
+}
---
+++ src/app/features/restore-password-2/restore-password-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restore-password-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './restore-password-2.component.html',
+  styleUrl: './restore-password-2.component.css'
+})
+export class RestorePassword2Component {
+
+}
