65b7b1ba11aa2cc1b8071b371553e491535fabc6
ChristianSp <christiansp7231@gmail.com>
2024-06-13 00:13:03 -0600

    Refactor dashboard routing to use separate file for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ForgetpassComponent } from './features/forgetpass/forgetpass.component'
 import { GeneralViewComponent } from './features/dashboard/general-view/general-view.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { SettingsComponent } from './features/settings/settings.component';
+import { DashboardRoutes } from './features/dashboard/dashboard.routes';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -12,7 +13,7 @@ export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'forgetpass', component: ForgetpassComponent },
     { path: 'general-view', component: GeneralViewComponent },
-    { path: 'dashboard', component: DashboardComponent },
+    ...DashboardRoutes,
     { path: 'settings', component: SettingsComponent },
     { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
