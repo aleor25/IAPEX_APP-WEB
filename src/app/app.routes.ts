a424f7f92805d190a543a8fa5592756310fa8eb6
unknown <tenchipe23>
2024-06-19 09:57:13 -0600

    se agregaron cambios a restore-password-2 y se agrego link en index de fonts, en el verified_email y en el correct-mail-verification se corrigio las fuentes y del logo los iconos los colores y alineamiento, se agrego autenticacion a los aprtados de register y login en restore-password-2 se corrigio los link de navegacion y su alineado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,11 @@ import { SettingsComponent } from './features/settings/settings.component';
 import { DashboardRoutes } from './features/dashboard/dashboard.routes';
 import { PacientesRegistradosComponent } from './features/dashboard/pacientes-registrados/pacientes-registrados.component';
 import { SolicitudesComponent } from './features/dashboard/solicitudes/solicitudes.component';
+import { RestorePassword2Component } from './features/restore-password-2/restore-password-2.component';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CorrectMailVerificationComponent } from './features/correct-mail-verification/correct-mail-verification.component';
+import { VerifiedEmailComponent } from './features/verified-email/verified-email.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -16,9 +21,15 @@ export const routes: Routes = [
     { path: 'forgetpass', component: ForgetpassComponent },
     { path: 'general-view', component: GeneralViewComponent },
     { path: 'pacientes-registrados', component: PacientesRegistradosComponent },
+
+    { path: 'restore-password-2', component: RestorePassword2Component},
+    { path: 'correct-mail-verification', component: CorrectMailVerificationComponent},
+    { path: 'verified-email', component: VerifiedEmailComponent},
+
     { path: 'solicitudes', component: SolicitudesComponent },
     ...DashboardRoutes,
     { path: 'settings', component: SettingsComponent },
     { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
+
 
