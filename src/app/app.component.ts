7cc323063324727ea1a2e83c44f814ddc5216c32
ChristianSp <christiansp7231@gmail.com>
2024-11-13 19:34:16 -0600

    Se implemento modo oscuro en toda la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from './shared/components/toast/toast.component';
+import { ThemeService } from './core/services/util/theme.service';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    imports: [RouterOutlet, ToastComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [RouterOutlet, ToastComponent]
 })
 export class AppComponent {
   title = 'IAPEX';
+
+  private _themeService = inject(ThemeService);
+
+  constructor() {
+    this._themeService.themeSignal();
+  }
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,18 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import { ThemeService } from '../../../core/services/util/theme.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  imports: [RouterLink, RouterLinkActive, NgClass],
+  templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent {
 
   private _router = inject(Router);
   private _authService = inject(AuthService);
+  public _themeService = inject(ThemeService);
+
+  toggleTheme() {
+    this._themeService.updateTheme();
+  }
 
   logout(): void {
     this._authService.logout();
