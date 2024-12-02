8619225927f3b12a664fe2ab52b2ebb007efba5a
ChristianSp <christiansp7231@gmail.com>
2024-12-01 23:57:33 -0600

    Refactor notifications component to use structural directives and improve HTML structure

--- src/app/features/dashboard/notifications/notifications.component.ts
+++ src/app/features/dashboard/notifications/notifications.component.ts
@@ -1,12 +1,10 @@
-import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [NgFor],
+  imports: [],
   templateUrl: './notifications.component.html'
 })
 export class NotificationsComponent {
-  notificaciones = new Array(10); // Array con 5 elementos
 }
